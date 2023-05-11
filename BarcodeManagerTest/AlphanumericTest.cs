1e2ea7a5f9069a22d537e30d1d985c306793cac8
Sebastian Agius <seb@agius.xyz>
2023-05-11 16:44:29 +1000

    Fix Tests and Test optimal format of checking an alphanumeric string

---
+++ BarcodeManagerTest/AlphanumericTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BarcodeManagerTest
+{
+    public class AlphanumericTest
+    {
+        char a = 'a', b = 'z', c = '!', d = '\t', e = ' ';
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void TestRegex()
+        {
+            Regex rg = new Regex(@"^[a-zA-Z0-9 ]");
+            Assert.IsTrue(rg.IsMatch(a.ToString()));
+            Assert.IsTrue(rg.IsMatch(b.ToString()));
+            Assert.IsFalse(rg.IsMatch(c.ToString()));
+            Assert.IsFalse(rg.IsMatch(d.ToString()));
+            Assert.IsTrue(rg.IsMatch(e.ToString()));
+        }
+
+        [Test]
+        public void TestString()
+        {
+            String x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+
+            Assert.IsTrue(x.Contains(a));
+            Assert.IsTrue(x.Contains(b));
+            Assert.IsFalse(x.Contains(c));
+            Assert.IsFalse(x.Contains(d));
+            Assert.IsTrue(x.Contains(e));
+        }
+
+        [Test]
+        public void TestChar()
+        {
+
+            Assert.IsTrue(char.IsLetterOrDigit(a));
+            Assert.IsTrue(char.IsLetterOrDigit(b));
+            Assert.IsFalse(char.IsLetterOrDigit(c));
+            Assert.IsFalse(char.IsLetterOrDigit(d));
+            Assert.IsFalse(char.IsLetterOrDigit(e));
+        }
+    }
+}
--- BarcodeManagerTest/CommandProcessorUnitTest.cs
+++ BarcodeManagerTest/CommandProcessorUnitTest.cs
@@ -35,11 +35,11 @@ namespace BarcodeManagerTest
         [Test]
         public void TestBasicAutoComplete()
         {
-            Assert.That(_processor.AutoComplete("sw")![0]!, Is.EqualTo("itch <stock/registry>"));
-            Assert.That(_processor.AutoComplete("switc")![0]!, Is.EqualTo("h <stock/registry>"));
-            Assert.That(_processor.AutoComplete("switch <st")![0]!, Is.EqualTo("ock/registry>"));
-            Assert.That(_processor.AutoComplete("s")![0]!, Is.EqualTo(""));
-            Assert.That(_processor.AutoComplete("blah")![0]!, Is.EqualTo(""));
+            Assert.That(_processor.AutoComplete("sw")![0]!, Is.EqualTo("itch stock"));
+            Assert.That(_processor.AutoComplete("switc")![0]!, Is.EqualTo("h stock"));
+            Assert.That(_processor.AutoComplete("switch st")![0]!, Is.EqualTo("ock"));
+            Assert.That(_processor.AutoComplete("s"), Is.Null);
+            Assert.That(_processor.AutoComplete("blah"), Is.Null);
         }
     }
 }
